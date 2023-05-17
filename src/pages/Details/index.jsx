3634feb94fee1fcb027c924a23162ba3df859143
Carlos Barboza <cadusb2@gmail.com>
2023-05-17 06:53:51 -0300

    Initializing the project and setting global styles

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+export function Details() {
+
+  return (
+    <Container>
+     <h1>Hello World!</h1>
+     <span>Carlos Eduardo</span>
+    </Container>
+  )
+}
+
+
