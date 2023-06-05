8bafdc16864e3f393b38ee56ba1797aff56d4645
Carlos Barboza <cadusb2@gmail.com>
2023-06-05 06:37:51 -0300

    Sign In page finished

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+import backgroundImg from "../../assets/images/background.jpg"
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`
+
+export const Form = styled.form`
+  padding: 0 136px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  text-align: left;
+
+  > h1 {
+    font-size: 48px;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  > h2 {
+    font-size: 24px;
+    margin-top: 84px;
+    margin-bottom: 24px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 124px;
+    color: ${({ theme }) => theme.COLORS.PINK};
+    align-self: center;
+  }
+`
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center;
+  background-size: cover;
+`
